c637600aa4714a3fab70c50a4842dbc76b9860a0
Darren Vandervort <darren@opencapitalist.org>
2015-01-22 15:29:53 -0800

    Created new firmware for Diskeepers with LEDS.
    Just a software based loop to light LEDs in a very simple pattern.  When
    code is refactored.

---
+++ Debug/mainLED.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+mainLED.obj: ../mainLED.c
+mainLED.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+mainLED.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430g2302.h
+mainLED.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+mainLED.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/intrinsics.h
+mainLED.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/intrinsics_legacy_undefs.h
+
+../mainLED.c: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430g2302.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/intrinsics.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/intrinsics_legacy_undefs.h: 
--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -3,10 +3,10 @@
 ################################################################################
 
 # Each subdirectory must supply rules for building sources it contributes
-main.obj: ../main.c $(GEN_OPTS) $(GEN_HDRS)
+mainLED.obj: ../mainLED.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv5/tools/compiler/msp430_4.2.3/bin/cl430" -vmsp --abi=eabi --include_path="C:/ti/ccsv5/ccs_base/msp430/include" --include_path="C:/ti/ccsv5/tools/compiler/msp430_4.2.3/include" --advice:power=all -g --define=__MSP430G2302__ --diag_warning=225 --display_error_number --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="main.pp" $(GEN_OPTS__FLAG) "$<"
+	"c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/bin/cl430" -vmsp --abi=eabi --use_hw_mpy=none --include_path="c:/ti/ccsv6/ccs_base/msp430/include" --include_path="c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include" --advice:power=all -g --define=__MSP430G2302__ --diag_warning=225 --display_error_number --diag_wrap=off --printf_support=minimal --preproc_with_compile --preproc_dependency="mainLED.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
